7d58af930751a8bcabba3120d1eeae78aa0ef73b
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-03-13 23:53:25 +0100

    can now filter users from tweets

--- helpers/twitter.js
+++ helpers/twitter.js
@@ -1,17 +1,26 @@
 let Twitter = require('twitter');
 
-let TWITTER_CONSUMER_KEY = "UZp1BOGMpYwtqPcePGDh4ODUv";
-let TWITTER_CONSUMER_SECRET = "vHu4f65E2BCk8urJ9IPCq5yDfV9F5aOx7EyGfrAbctlRfIbsNV";
-let TWITTER_ACCESS_TOKEN = "2481818113-j1NjF6wB6Ko9crDKUrUo2LA5nMEyqlflZvg3Zqb";
-let TWITTER_ACCESS_TOKEN_SECRET = "haCvnGZ79G1e5nc9xlKGH56DOLKwXBbZXJASdQO8gHs5W";
-
 let client = new Twitter({
-    consumer_key: TWITTER_CONSUMER_KEY,
-    consumer_secret: TWITTER_CONSUMER_SECRET,
-    access_token_key: TWITTER_ACCESS_TOKEN,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets) {
-    console.log(tweets.statuses.length);
- });
+const getTweets = (hashTags) => {
+    return new Promise((resolve, reject) => {
+        let searchQuery = hashTags.join(" OR ");
+        client.get('search/tweets', {
+            q: searchQuery, count: process.env.MAX_COUNT
+        }, function (error, tweets) {
+            if (error) {
+                reject(Error(error));
+            } else {
+                resolve(tweets);
+            }
+        });
+    });
+};
+
+
+module.exports = getTweets;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+let formatInput = require("../helpers/formatInput");
+let getTweets = require("../helpers/twitter");
+let filterUsers = require("../helpers/filterUsers");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Twitter Bot' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Twitter Bot'
+  });
 });
 
 /* POST to home page. */
-router.post('/', function(req, res, next) {
-  
+router.post('/', function (req, res, next) {
+  let formattedHashTags = formatInput(req.body.twitterHashTags);
+  getTweets(formattedHashTags).then(tweets => filterUsers(tweets.statuses))
+  .catch((error) => console.log(error));
 });
 
 module.exports = router;
