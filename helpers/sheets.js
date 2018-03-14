9b1aacbab78ce065f6625b28af20554a9e9faf62
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-03-14 23:14:00 +0100

    gets realtime data

--- helpers/sheets.js
+++ helpers/sheets.js
@@ -3,23 +3,26 @@ let google = require("./google");
 let sheets = googleapis.sheets('v4');
 
 let writeToSheet = (userData) => {
-    google.checkToken().then((auth) => {
-        sheets.spreadsheets.values.update({
-            auth: auth,
-            spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
-            range: 'names!A2:D50',
-            valueInputOption: 'USER_ENTERED',
-            resource: {
-                values: userData,
-                majorDimension: 'ROWS'
-            }
-        }, (err, response) => {
-            if (err) {
-                console.log(err);
-                return;
-            } else {
-                console.log("DONE!");
-            }
+    return new Promise((resolve, reject) => {
+        google.checkToken().then((auth) => {
+            sheets.spreadsheets.values.append({
+                auth: auth,
+                spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+                range: 'names!A2:D500',
+                valueInputOption: 'USER_ENTERED',
+                insertDataOption: 'INSERT_ROWS',
+                resource: {
+                    values: userData,
+                    majorDimension: 'ROWS'
+                }
+            }, (err, response) => {
+                if (err) {
+                    reject(Error(err));
+                } else {
+                    resolve("DONE!");
+                }
+            });
+
         });
 
     });
--- routes/success.js
+++ routes/success.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
-let twitter = require("../helpers/twitter");
+let Twitter = require('twitter');
+
+let client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+// let twitter = require("../helpers/twitter");
 let formatInput = require("../helpers/formatInput");
 let filterUsers = require("../helpers/filterUsers");
 let sheets = require("../helpers/sheets");
@@ -9,19 +17,37 @@ let sheets = require("../helpers/sheets");
 router.get('/', function (req, res, next) {
     let hashTags = req.session.hashTags;
     let formattedHashTags = formatInput(hashTags);
-    let userData;
+    let searchQuery, stream;
 
-    twitter(formattedHashTags)
-        .then((tweets) => {
-            userData = filterUsers(tweets.statuses);
-            return userData;
-        }).then(() => sheets(userData))
-        .then(() => {
-            res.render('success', {
-                title: 'Twitter Bot'
+    if(formattedHashTags.length <2) {
+        searchQuery = formattedHashTags[0];
+    }
+    else {
+        searchQuery = hashTags.join(" OR ");
+    }
+
+    client.stream('statuses/filter', {track: searchQuery}, (stream) => {
+        stream.on('data', function(tweets) {
+            let userData =[];
+            userData[0] = tweets.user.screen_name;
+            userData[1] = tweets.user.name;
+            userData[2] = tweets.user.followers_count;
+
+            client.currentTweetStream = stream;
+            sheets([userData]).catch((err) => {
+                console.log(err);
+                stream.destroy();
             });
-        })
-        .catch(error => console.log(error));
+        });
+        stream.on('error', function(error) {
+            console.log(error);
+          });
+
+    });
+
+    res.render('success', {
+        title: 'Twitter Bot'
+    });
 
 });
 
